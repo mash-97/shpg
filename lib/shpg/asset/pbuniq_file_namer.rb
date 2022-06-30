dc5cbcf1269a6ead75ba50b4dbbf23152ff4faa1
mash-97 <mahimnhd97@gmail.com>
2022-07-01 03:57:46 +0600

    format&doc

--- lib/shpg/asset/pbuniq_file_namer.rb
+++ lib/shpg/asset/pbuniq_file_namer.rb
@@ -1,108 +1,103 @@
 # asset/pbuniq_file_namer.rb
-   
+
 module Shpg
   module Asset
     class PastableArray < Array
       def paste(glue)
-	temp = []
-	index = 0
-	while index+1 < size do 
-	  temp << self[index]
-	  temp << glue
-	  index += 1
-	end
-	temp << self[size-1]
+        temp = []
+        index = 0
+        while index + 1 < size
+          temp << self[index]
+          temp << glue
+          index += 1
+        end
+        temp << self[size - 1]
       end
     end
 
     class PBUniqFileNamer
       NUM_TRY_LIMIT = 1000
-      
+
       def initialize(dir_path, file_path)
-	@dir_path = dir_path
-	@file_path = file_path
-	@file_name = File.basename(file_path)
+        @dir_path = dir_path
+        @file_path = file_path
+        @file_name = File.basename(file_path)
       end
-      
-      def getUniqFilename()
-	return self.class.get_safe_uniq_filename(Dir.entries(@dir_path), @file_path)
+
+      def getUniqFilename
+        self.class.get_safe_uniq_filename(Dir.entries(@dir_path), @file_path)
       end
-      
+
       def self.get_trimmed_path_sequence(file_dir_path, relative_base_path)
-	sequence = file_dir_path.split(relative_base_path)
-	if sequence.size > 1  then
-	  sequence.shift # remove the first part
-	  sequence = PastableArray(sequence).paste(relative_base_path)
-	end
-	sequence = File.join(sequence).split(File::Separator)
-	return sequence
+        sequence = file_dir_path.split(relative_base_path)
+        if sequence.size > 1
+          sequence.shift # remove the first part
+          sequence = PastableArray(sequence).paste(relative_base_path)
+        end
+        File.join(sequence).split(File::Separator)
       end
-      
+
       def self.get_uniq_filename_by_path(dir_entries, file_name, path_sequence)
-	index = 0
-	
-	while index < path_sequence.size do 
-	  file_name = path_sequence[index]+"_"+file_name
-	  return file_name if not dir_entries.include?(file_name)
-	  index += 1
-	end
-	
-	return nil
+        index = 0
+
+        while index < path_sequence.size
+          file_name = path_sequence[index] + "_" + file_name
+          return file_name unless dir_entries.include?(file_name)
+
+          index += 1
+        end
+
+        nil
       end
-    
-      def self.get_uniq_filename_by_num(dir_entries, file_name, path_sequence, try_limit=NUM_TRY_LIMIT)
-	file_name = [path_sequence.join("_"), file_name].join("_")
-	try_limit.times do |i|
-	  tempfn = ""
-	  if File.extname(file_name).length>0 then
-	    tempfn = (file_name.split(File.extname(file_name))[0])+(i+1).to_s+File.extname(file_name)
-	  else
-	    tempfn = file_name+(i+1).to_s
-	  end
-	  return tempfn if not dir_entries.include?(tempfn)
-	end
-	return nil
+
+      def self.get_uniq_filename_by_num(dir_entries, file_name, path_sequence, try_limit = NUM_TRY_LIMIT)
+        file_name = [path_sequence.join("_"), file_name].join("_")
+        try_limit.times do |i|
+          tempfn = ""
+          tempfn = if File.extname(file_name).length > 0
+                     (file_name.split(File.extname(file_name))[0]) + (i + 1).to_s + File.extname(file_name)
+                   else
+                     file_name + (i + 1).to_s
+                   end
+          return tempfn unless dir_entries.include?(tempfn)
+        end
+        nil
       end
-      
-      def self.get_uniq_filename(dir_entries, file_path, relative_base_path=nil)
-	
-	path_sequence = File.split(File.absolute_path(file_path))
-	file_name = path_sequence[1]
-	path_sequence = path_sequence[0]
-	path_sequence = get_trimmed_path_sequence(path_sequence, File.absolute_path(relative_base_path))
-	
-	result = get_uniq_filename_by_path(dir_entries, file_name, path_sequence)
-	if(result==nil) then
-	  result = get_uniq_filename_by_num(dir_entries, file_name, path_sequence)
-	end
-	return result
+
+      def self.get_uniq_filename(dir_entries, file_path, relative_base_path = nil)
+        path_sequence = File.split(File.absolute_path(file_path))
+        file_name = path_sequence[1]
+        path_sequence = path_sequence[0]
+        path_sequence = get_trimmed_path_sequence(path_sequence, File.absolute_path(relative_base_path))
+
+        result = get_uniq_filename_by_path(dir_entries, file_name, path_sequence)
+        result = get_uniq_filename_by_num(dir_entries, file_name, path_sequence) if result.nil?
+        result
       end
-      
+
       def self.get_safe_uniq_filename(dir_entries, file_name, try_limit = NUM_TRY_LIMIT)
-	
-	try_limit.times do |i|
-	  tempfn = ""
-	  if File.extname(file_name).length>0 then
-	    tempfn = (file_name.split(File.extname(file_name))[0])+(i+1).to_s+File.extname(file_name)
-	  else
-	    tempfn = file_name+(i+1).to_s
-	  end
-	  return tempfn if not dir_entries.include?(tempfn)
-	end
-	return nil
+        try_limit.times do |i|
+          tempfn = ""
+          tempfn = if File.extname(file_name).length > 0
+                     (file_name.split(File.extname(file_name))[0]) + (i + 1).to_s + File.extname(file_name)
+                   else
+                     file_name + (i + 1).to_s
+                   end
+          return tempfn unless dir_entries.include?(tempfn)
+        end
+        nil
       end
-	
     end
   end
 end
 
-if $0 == __FILE__ then
-  dir_entries = ['a', 'b', 'c', 'a.txt', '.', '..']
-  file_paths = ['a', 'a/a/a', 'b', 'b/c', 'a.txt', 'e/a.txt', 'e', 'd.txt']
+if $0 == __FILE__
+  dir_entries = ["a", "b", "c", "a.txt", ".", ".."]
+  file_paths = ["a", "a/a/a", "b", "b/c", "a.txt", "e/a.txt", "e", "d.txt"]
   file_paths.each do |file_path|
     print("For file_path: #{file_path} --> ")
     u = Shpg::AssetManager::PBUniqFileNamer.get_safe_uniq_filename(dir_entries, file_path)
-    if(u!=nil) then
+    unless u.nil?
       dir_entries << u
       puts(u)
     end
--- lib/shpg/asset/placer.rb
+++ lib/shpg/asset/placer.rb
@@ -1,80 +1,79 @@
 # asset/placer.rb
 
-#~ require 'yaml'
-#~ require 'tempfile'
+# ~ require 'yaml'
+# ~ require 'tempfile'
 require "fileutils"
 
 require_relative("asset")
 require_relative("file_checksum")
 require_relative("pbuniq_file_namer")
 
-module Shpg 
+module Shpg
   module Asset
-    
     class Placer
       class UnableToProduceDestFilePath < StandardError
-	def initialize(message, dest_dir, file_path)
-	  @dest_dir = dest_dir
-	  @file_path = file_path
-	  super(message)
-	end
+        def initialize(message, dest_dir, file_path)
+          @dest_dir = dest_dir
+          @file_path = file_path
+          super(message)
+        end
       end
-      
-      attr_accessor :container
-      attr_accessor :dest_dir
-      attr_accessor :yaml_file
-      
+
+      attr_accessor :container, :dest_dir, :yaml_file
+
       def initialize(dest_dir)
-	@dest_dir = dest_dir
-	#~ @yaml_file = Tempfile.new(["container", ".yml"])
-	@container = []
-	#~ YAML.dump(@container, @yaml_file)
+        @dest_dir = dest_dir
+        # ~ @yaml_file = Tempfile.new(["container", ".yml"])
+        @container = []
+        # ~ YAML.dump(@container, @yaml_file)
       end
-      
+
       def includeAsset(asset_path)
-	asset_checksum = Shpg::Asset::FileChecksum.get_checksum(asset_path)
-	dest_path = getPathFromContainer(asset_checksum)
-	
-	if not dest_path then
-	  filename = File.basename(asset_path)
-	  if File.exist?(File.join(@dest_dir, filename)) then
-	    filename = Shpg::Asset::PBUniqFileNamer.get_safe_uniq_filename(Dir.entries(@dest_dir), filename, try_limit=3000)
-	  end
-	  dest_path = File.join(@dest_dir, filename) if filename
-	else
-	  return dest_path
-	end
-	
-	# I get the feel I need to remove this logic
-	# if no dest_path will be unabled to find with the current configs
-	raise UnableToProduceDestFilePath.new("Unable to find a file path to avoid filename conflict!", @dest_dir, asset_path) if not dest_path
-	
-	# place the asset_path file at the @dest_dir with the new name dest_path name
-	FileUtils.copy(asset_path, dest_path)
-	dest_checksum = Shpg::Asset::FileChecksum.get_checksum(asset_path)
-	return "" if not File.exist?(dest_path) or dest_checksum!=asset_checksum
-	
-	asset_data = {:path => dest_path, :old_path => asset_path, :checksum => asset_checksum}
-	@container << asset_data
-	#~ dumpContainerInYaml()
-	
-	return asset_data[:path]
+        asset_checksum = Shpg::Asset::FileChecksum.get_checksum(asset_path)
+        dest_path = getPathFromContainer(asset_checksum)
+
+        if !dest_path
+          filename = File.basename(asset_path)
+          if File.exist?(File.join(@dest_dir, filename))
+            filename = Shpg::Asset::PBUniqFileNamer.get_safe_uniq_filename(Dir.entries(@dest_dir), filename,
+                                                                           try_limit = 3000)
+          end
+          dest_path = File.join(@dest_dir, filename) if filename
+        else
+          return dest_path
+        end
+
+        # I get the feel I need to remove this logic
+        # if no dest_path will be unabled to find with the current configs
+        unless dest_path
+          raise UnableToProduceDestFilePath.new("Unable to find a file path to avoid filename conflict!", @dest_dir,
+                                                asset_path)
+        end
+
+        # place the asset_path file at the @dest_dir with the new name dest_path name
+        FileUtils.copy(asset_path, dest_path)
+        dest_checksum = Shpg::Asset::FileChecksum.get_checksum(asset_path)
+        return "" if !File.exist?(dest_path) || (dest_checksum != asset_checksum)
+
+        asset_data = { path: dest_path, old_path: asset_path, checksum: asset_checksum }
+        @container << asset_data
+        # ~ dumpContainerInYaml()
+
+        asset_data[:path]
       end
-      
+
       private
-      #~ def dumpContainerInYaml()
-	#~ YAML.dump(@container, File.open(@yaml_file_path))
-      #~ end
-	
+
+      # ~ def dumpContainerInYaml()
+      # ~ YAML.dump(@container, File.open(@yaml_file_path))
+      # ~ end
+
       def getPathFromContainer(checksum)
-	@container.each do |hash|
-	  if hash[:checksum]==checksum then
-	    return hash[:path]
-	  end
-	end
-	return nil
+        @container.each do |hash|
+          return hash[:path] if hash[:checksum] == checksum
+        end
+        nil
       end
     end
   end
 end
-
